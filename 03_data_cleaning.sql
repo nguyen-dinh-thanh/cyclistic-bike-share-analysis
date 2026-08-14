dfdbf042ca3d72b75aa51b305c70f38bb77c29dc
nguyen-dinh-thanh <nemo.opt@gmail.com>
2026-08-14 20:58:09 +0700

    Create 03_data_cleaning.sql

---
+++ 03_data_cleaning.sql
@@ -0,0 +1,37 @@
+CREATE OR REPLACE TABLE `stable-ring-382611.cyclistic_case_study.cleaned_tripdata` AS
+
+WITH deduplicated_and_casted AS (
+  SELECT DISTINCT
+    ride_id,
+    rideable_type,
+    CAST(started_at AS TIMESTAMP) AS started_at,
+    CAST(ended_at AS TIMESTAMP) AS ended_at,
+    start_station_name,
+    end_station_name,
+    start_lat,
+    start_lng,
+    end_lat,
+    end_lng,
+    member_casual
+  FROM `stable-ring-382611.cyclistic_case_study.combined_tripdata`
+),
+engineered_features AS (
+  SELECT 
+    *,
+    TIMESTAMP_DIFF(ended_at, started_at, MINUTE) AS duration_minutes,
+    FORMAT_TIMESTAMP('%A', started_at) AS day_of_week,
+    FORMAT_TIMESTAMP('%B', started_at) AS month,
+    EXTRACT(HOUR FROM started_at) AS start_hour
+  FROM deduplicated_and_casted
+)
+SELECT *
+FROM engineered_features
+WHERE 
+  end_lat IS NOT NULL 
+  AND end_lng IS NOT NULL
+  AND duration_minutes >= 1 
+  AND duration_minutes <= 1440
+  AND COALESCE(LOWER(start_station_name), '') NOT LIKE '%test%'
+  AND COALESCE(LOWER(end_station_name), '') NOT LIKE '%test%'
+  AND end_lat BETWEEN 41.6 AND 42.1 
+  AND end_lng BETWEEN -87.9 AND -87.5
